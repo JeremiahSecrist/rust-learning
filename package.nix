2da1da4d99331c4e9e9e1ee8939f2064792c9e70
Jermeiah S <owner@arouzing.xyz>
2024-02-18 15:09:06 -0500

    cleanup: removed rec

--- package.nix
+++ package.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> { } }:
 
-pkgs.rustPlatform.buildRustPackage rec {
+pkgs.rustPlatform.buildRustPackage {
   pname = "hello_world";
   version = "0.1.0";
   cargoLock.lockFile = ./Cargo.lock;
