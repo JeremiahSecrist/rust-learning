0ffb2f5f2a7a6cee471170433a38767eddc07eb5
Jermeiah S <owner@arouzing.xyz>
2024-02-18 22:38:44 -0500

    trying iced.rs

--- package.nix
+++ package.nix
@@ -1,10 +1,57 @@
-{rustPlatform, lib }:
+{rustPlatform,
+ lib,
+ libGL,
+ bzip2,
+ fontconfig,
+ freetype,
+ libX11,
+ libXcursor,
+ libXrandr,
+ libXi,
+ vulkan-loader,
+ gnome,
+ libsForQt5,
+ cmake,
+ pkg-config,
+ makeWrapper
+ }:
 rustPlatform.buildRustPackage {
   pname = "hello_world";
   version = "0.1.0";
   cargoLock.lockFile = ./Cargo.lock;
   src = lib.cleanSource ./.;
-  buildInputs = [];
-  nativeBuildInputs = [];
+  postFixup =
+    let
+      libPath = lib.makeLibraryPath [
+        libGL
+        bzip2
+        fontconfig
+        freetype
+        libX11
+        libXcursor
+        libXrandr
+        libXi
+        vulkan-loader
+      ];
+    in
+    ''
+      patchelf --set-rpath "${libPath}" "$out/bin/$pname"
+      wrapProgram $out/bin/$pname --prefix PATH : ${lib.makeBinPath [ gnome.zenity libsForQt5.kdialog ]}
+    '';
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    makeWrapper
+  ];
+
+  buildInputs = [
+    fontconfig
+    freetype
+    libX11
+    libXcursor
+    libXrandr
+    libXi
+  ];
+
   #  doCheck = false;
 }
