5f1834215f42dcaf15e82e8a7dbb1055f5ee43e9
Jermeiah S <owner@arouzing.xyz>
2024-02-20 13:34:26 -0500

    trying arg parsing

--- package.nix
+++ package.nix
@@ -1,56 +1,25 @@
 {rustPlatform,
  lib,
- libGL,
- bzip2,
  fontconfig,
- freetype,
- libX11,
- libXcursor,
- libXrandr,
- libXi,
- libxkbcommon,
- vulkan-loader,
- expat,
- gnome,
- libsForQt5,
  cmake,
  pkg-config,
  makeWrapper
  }:
- let
-  runtimeLibs = [
-    expat
-    fontconfig
-    freetype
-    freetype.dev
-    libGL
-    vulkan-loader
-    libX11
-    libXcursor
-    libXi
-    libXrandr
-    libxkbcommon
-  ];
-in
+
 rustPlatform.buildRustPackage {
   pname = "hello_world";
   version = "0.1.0";
   cargoLock.lockFile = ./Cargo.lock;
   src = lib.cleanSource ./.;
-  postFixup =
-      # patchelf --set-rpath "${libPath}" "$out/bin/$pname"
-    ''
-      wrapProgram  $out/bin/$pname --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath runtimeLibs}"
-    '';
   nativeBuildInputs = [
     cmake
     pkg-config
     makeWrapper
   ];
-
+  
+  CARGO_INCREMENTAL = "0";
   buildInputs = [
     fontconfig
-    # systemd
   ];
 
   #  doCheck = false;
--- shell.nix
+++ shell.nix
@@ -1,9 +1,25 @@
 {pkgs ? import <nixpkgs> {}}:
+let
+runtimeLibs = with pkgs; [
+    expat
+    fontconfig
+    freetype
+    freetype.dev
+    libGL
+    vulkan-loader
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXrandr
+    libxkbcommon
+  ];
+in
 pkgs.mkShell {
   # Get dependencies from the main package
   inputsFrom = [(pkgs.callPackage ./package.nix {})];
   RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
   # Additional tooling
+  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath runtimeLibs}";
   buildInputs = with pkgs; [
     cargo
     cargo-watch
