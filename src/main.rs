51917f22681bc5cb5a8cb00ca403525d86d23e28
Jermeiah S <owner@arouzing.xyz>
2024-02-22 03:36:22 -0500

    feat: removed hashing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use clap::Parser;
 use shunting::*;
-use hsh::models::hash::Hash;
-use base64::{engine::general_purpose::STANDARD, Engine as _};
 
 /// Simple program to solve equations
 #[derive(Parser)]
@@ -14,10 +12,22 @@ struct Args {
 fn main() {
     let args = Args::parse();
     let input = args.value;
-    let expr = ShuntingParser::parse_str(&input)
-        .expect("Failed to shunt value");
-    let result = MathContext::new()
-        .eval(&expr)
-        .expect("Failed to calculate shunted value");
+
+    let expr = match ShuntingParser::parse_str(&input) {
+        Ok(expr) => expr,
+        Err(err) => {
+            eprintln!("Failed to shunt value: {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let result = match MathContext::new().eval(&expr) {
+        Ok(result) => result,
+        Err(err) => {
+            eprintln!("Failed to calculate shunted value: {}", err);
+            std::process::exit(1);
+        }
+    };
+
     println!("{} = {}", &input, result);
 }
