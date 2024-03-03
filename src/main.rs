78baa2abbde15467a7cc53db414b8ef4e7b86bc5
Jermeiah S <owner@arouzing.xyz>
2024-03-03 18:42:22 -0500

    shifting to new goal

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,5 @@ struct Args {
 }
 
 fn main() {
-    let args = Args::parse();
-    let input = args.value;
 
-    let expr = match ShuntingParser::parse_str(&input) {
-        Ok(expr) => expr,
-        Err(err) => {
-            eprintln!("Failed to shunt value: {}", err);
-            std::process::exit(1);
-        }
-    };
-
-    let result = match MathContext::new().eval(&expr) {
-        Ok(result) => result,
-        Err(err) => {
-            eprintln!("Failed to calculate shunted value: {}", err);
-            std::process::exit(1);
-        }
-    };
-
-    println!("{} = {}", &input, result);
 }
